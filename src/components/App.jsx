38a81000f9627089ac770fbfda65cc68c58d5943
ericvegax <mcpe2004salsa@gmail.com>
2023-12-14 18:34:33 -0500

    added styles to Hero component (still in progress).

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./Hero.jsx";
 function App() {
     return (
         <div className={"container"}>
-            <Hero title={""} button={""} />
+            <Hero span={"I'm Eric"} title={", a creative web developer with 4 years of inovating life with code."} button={"let's get in touch."} />
         </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 function Hero(props) {
-    return (
-        <div className={""}>
-            <h2>{props.title}</h2>
-            <a href={"#"}>{props.button}</a>
-        </div>
-    );
+    if (props.span === null) {
+        return (
+            <div className={"container hero"}>
+                <h2>{props.title}</h2>
+                <a href={"#"}>{props.button}</a>
+            </div>
+        );
+    } else {
+        return (
+            <div className={"container hero"}>
+                <h2><span>{props.span}</span> {props.title}</h2>
+                <a href={"#"}>{props.button}</a>
+            </div>
+        );
+    }
 }
 export default Hero;
