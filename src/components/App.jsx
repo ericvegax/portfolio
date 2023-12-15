bb4b0f0ab56bd38c0a49955cdc463da4fd8d27e0
ericvegax <mcpe2004salsa@gmail.com>
2023-12-15 12:51:31 -0500

    added favicon & changed title.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./Hero.jsx";
+import Footer from "./Footer.jsx";
 function App() {
     return (
         <div className={"container"}>
-            <Hero span={"I'm Eric"} title={", a Passionate Web Artisan with 3 Years of Crafting Digital Solutions."} button={"let's get in touch."} />
+            <Hero span={"I'm Eric"} title={", a Passionate Web Artisan Crafting Digital Solutions."} button={"let's get in touch."} />
+            <Footer />
         </div>
     )
 }
